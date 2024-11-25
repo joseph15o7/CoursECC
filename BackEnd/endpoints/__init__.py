8fcb890b1d5dd2f8f22682bd3f0b12466980f0ba
joseph15o7 <youssef.ouzeroual.1507@gmail.com>
2024-11-25 11:42:18 +0100

    init

---
+++ BackEnd/endpoints/__init__.py
@@ -0,0 +1,6 @@
+from endpoints.AuthApi import auth_ns
+from endpoints.CategoryApi import category_ns
+from endpoints.CourseApi import course_ns
+from endpoints.TopicApi import topic_ns
+from endpoints.TutorialApi import tutorial_ns
+from endpoints.EnrolledCourseApi import enrolled_course_ns
